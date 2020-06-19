43862e9da897e8fc23b2cc47a5a7a0ee1f2d17f2
Lukas Krauch <lukas.krauch@yahoo.de>
2020-06-19 18:27:30 +0200

    Remove comment

--- src/index.js
+++ src/index.js
@@ -30,7 +30,7 @@ const createWindow = () => {
 
 
     // Open the DevTools.
-    mainWindow.webContents.openDevTools();
+    // mainWindow.webContents.openDevTools();
 };
 
 const mainMenuTemplate = [];
