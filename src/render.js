6c3d8bdeac43d0ad4ff0255182670a0fdb28b0d9
Lukas Krauch <lukas.krauch@yahoo.de>
2020-06-21 16:10:36 +0200

    Finish up application

--- src/render.js
+++ src/render.js
@@ -5,10 +5,34 @@ const nlp = require('./fake_nlp');
 const suggestion = require('./components/suggestion');
 
 const textArea = document.getElementById('mainTextArea');
+const textInfo = document.getElementById('text-info');
+textInfo.onclick = function () {
+    dialog.showMessageBox(null, {
+        type: "info",
+        title: "Information",
+        message: "Dieser Text wird durch ein 'Fake NLP'-Model analysiert. Das Ergebnis wird an die " +
+            "API von 'https://de.openlegaldata.io' geschickt, um die Paragraphen zu erhalten. Zu dem " +
+            "aktuellen Zeitpunkt wird außerdem nur das Strafgesetzbuch abgefragt. Wenn kein Ergebnis gefunden wird, " +
+            "wird als Beispiel ein festes Ergebnis ausgegeben.\n\n" +
+            "Durch das Markieren von Sätzen oder Wörtern kann der Bereich der analysiert werden soll, eingeschränkt werden.",
+    });
+}
+
 const suggestionList = document.getElementById('suggestionList');
 
 const checkboxKeywords = document.getElementById('toggleKeywords');
 checkboxKeywords.onclick = toggleKeywords;
+
+const keywordInfo = document.getElementById('keyword-info');
+keywordInfo.onclick = function () {
+    dialog.showMessageBox(null, {
+        type: "info",
+        title: "Information",
+        message: "Diese Checkbox schält zwischen Keyword- und Paragraphensuche um. Bei einer Keywordsuche, " +
+            "werden die Texte der Paragraphen nach einem Keyword durchsucht und nur die ausgegeben, die das Wort beinhalten. " +
+            "Bei einer Paragraphensuche werden explizit die Paragraphen gesucht, die das NLP-Model anhand eines Keywords ermittelt hat.",
+    });
+}
 
 const checkBtn = document.getElementById('checkTextBtn');
 checkBtn.onclick = getSuggestions;
@@ -16,6 +40,14 @@ checkBtn.onclick = getSuggestions;
 const searchParagraphBtn = document.getElementById('search-paragraph');
 searchParagraphBtn.onclick = searchByParagraph;
 
+const searchInfo = document.getElementById('search-info');
+searchInfo.onclick = function () {
+    dialog.showMessageBox(null, {
+        type: "info",
+        title: "Information",
+        message: "Trenne mehrere Paragraphen durch ein Komma.",
+    });
+}
 
 const detailsClose = document.getElementById('details-close');
 detailsClose.onclick = function () {
@@ -31,6 +63,12 @@ let keywordSearch = true;
 function searchByParagraph() {
     const searchInput = document.getElementById('paragraph-input');
     let query = searchInput.value;
+
+    if (query === "") {
+        dialog.showErrorBox("Eingabefehler",
+            "Bitte geben sie mindestens einen Paragraphen an.");
+        return;
+    }
 
     const paragraphs = lawApi.fetchLaws(nlp.lawBook, query, '');
 
@@ -114,9 +152,13 @@ function addEventToItems() {
 }
 
 async function getSuggestions() {
-    const text = textArea.value;
-    // TODO only get marked text if marked
-    // TODO Add as Info-Icon
+    let text = textArea.value;
+
+    let selectedText = window.getSelection().toString();
+    console.log(selectedText.length);
+    if (selectedText.length > 0) {
+        text = selectedText;
+    }
 
     // Search (marked) text for keywords.
     const results = nlp.searchForKeywords(text);
@@ -171,19 +213,19 @@ function toggleKeywords() {
 let exampleText = "Um 17:45 Uhr am 17.06.2020 erfolgte die Anweisung durch das FLZ die Siberstraße 19 " +
     "in 71253 Entenhausen anzufahren. Laut einem Anrufer waren dort 2 Männer aneinandergeraten, was " +
     "anschließend in eine körperliche Auseinandersetzung übergegangen wäre. Die Anfahrt erfolgte unter Zuhilfenahme " +
-    "der Sonder- und Wegerechte ($35, $38 StVO).  Vor Ort konnten tatsächlich 2 Männer angetroffen werden, " +
+    "der Sonder- und Wegerechte (§35, §38 StVO).  Vor Ort konnten tatsächlich 2 Männer angetroffen werden, " +
     "welche sich vor der Einfahrt des Hauses Nr. 19 der Siberstraße schlugen. Dabei lag der Geschädigte Lauditz " +
     "bereits am Boden und versuchte sich vor den Tritten des Beschuldigten Kessler mit den Armen zu schützen.  " +
     "Da der Beschuldigte Kessler auf keinerlei Zurufe reagierte seine Schläge einzustellen, wurde er umgehend von " +
     "meinem Kollegen M.Hammer mithilfe eines Schockschlages zu Boden gebracht. Kurz darauf versuchte der " +
     "Beschuldigte Kessler zu flüchten und" +
-    " wurde deshalb von mir und meinem Kollegen M. Hammer umgehend vorläufig festgenommen ($127 StPO). Es erfolgte " +
-    "die Belehrung des Beschuldigten gemäß $136 StPO. Der Geschädigte wurde zu seinen Rechten als " +
+    " wurde deshalb von mir und meinem Kollegen M. Hammer umgehend vorläufig festgenommen (§127 StPO). Es erfolgte " +
+    "die Belehrung des Beschuldigten gemäß §136 StPO. Der Geschädigte wurde zu seinen Rechten als " +
     "Zeugen belehrt. Er erlitt lediglich leichte Prellungen seitlich der rechten " +
     "Rippen sowie Abschürfungen an beiden Unterarmen. Seinen Aussagen zufolge handelte es sich bei dem Beschuldigten" +
     " Kessler um seinen Nachbarn, welcher sich schon seit Wochen darüber aufregte, dass der Geschädigte Lauditz sein " +
     "PKW abends als so parke, dass die Schnauze des Fahrzeuges von der Einfahrt aus noch in den Gehweg hineinrage. " +
-    "Er wünsche einen Strafantrag für die vorliegende Körperverletzung ($223 StGB). Es erfolgte die Aufnahme der " +
+    "Er wünsche einen Strafantrag für die vorliegende Körperverletzung (§223 StGB). Es erfolgte die Aufnahme der " +
     "Personalien der Beteiligten, der Geschädigte Lauditz betrat anschließend sein Haus.  " +
     "Der Beschuldigte Kessler wurde zu seiner Haustüre begleitet, er zeigte sich einsichtig und wurde von seiner " +
     "Frau in Empfang genommen. ";
