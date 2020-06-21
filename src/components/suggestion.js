9eb2c41afe2cd8f8bbd83d560a3fa9fe69e5a198
Lukas Krauch <lukas.krauch@yahoo.de>
2020-06-21 09:40:13 +0200

    Add onClick event to show details

--- src/components/suggestion.js
+++ src/components/suggestion.js
@@ -8,7 +8,7 @@ function showDetails() {
 
 }
 
-function _buildSuggestionItem(title, text, maxLength = 60) {
+function _buildSuggestionItem(title, text, index) {
     const splitTitle = title.split(' ');
     // Get the next (add 2 because upper limit of splice is exclusive).
     let idx = splitTitle.indexOf('§') + 2;
@@ -16,8 +16,7 @@ function _buildSuggestionItem(title, text, maxLength = 60) {
     let paragraph = splitTitle.slice(0, idx).join(' ');
     let paragraphTitleText = splitTitle.slice(idx).join(' ');
 
-    let truncatedText = text.substr(0, maxLength) + " [...]";
-    return '<a class="list-group-item list-group-item-action border-left-0 border-right-0 border-top-0" id="suggestionItem">\n' +
+    return '<a class="list-group-item list-group-item-action border-left-0 border-right-0 border-top-0" id="suggestionItem' + index + '">\n' +
         '<h5 class="card-title">' + paragraph + '</h5>\n' +
         '<h6 class="card-subtitle mb-2 text-muted">' + paragraphTitleText + '</h6>\n' +
         '<p class="mb-0">' + text + '</p>\n' +
--- src/render.js
+++ src/render.js
@@ -1,21 +1,36 @@
-const electron = require('electron');
 const lawApi = require('./fetch_laws');
 const nlp = require('./fake_nlp');
 const suggestion = require('./components/suggestion');
 
 const textArea = document.getElementById('mainTextArea');
 const suggestionList = document.getElementById('suggestionList');
-const detailCard = document.getElementById('details-card');
 
 const checkBtn = document.getElementById('checkTextBtn');
-
 checkBtn.onclick = getSuggestions;
 
-function createEntryInSuggestions(title, text) {
-    let newSuggestion = suggestion.buildSuggestionItem(title, text, 90);
+
+function createEntryInSuggestions(title, text, idx) {
+    let newSuggestion = suggestion.buildSuggestionItem(title, text, idx);
 
     suggestionList.innerHTML += newSuggestion;
-    detailCard.hidden = false;
+}
+
+function addEventToItems() {
+    const detailCard = document.getElementById('details-card');
+    const detailsTitle = document.getElementById('details-title');
+    const detailsText = document.getElementById('details-text');
+    for (let c = 0; c < suggestionList.childElementCount; c++) {
+        let item = suggestionList.children[c]
+        let title = item.children[0].innerText;
+        let subtitle = item.children[1].innerText;
+        let text = item.children[2].innerText;
+
+        item.onclick = function () {
+            detailsTitle.innerText = title + " " + subtitle;
+            detailsText.innerText = text;
+            detailCard.hidden = false;
+        }
+    }
 }
 
 async function getSuggestions() {
@@ -42,14 +57,18 @@ async function getSuggestions() {
                 }
 
                 for (let i = 0; i < p.length; i++) {
-                    createEntryInSuggestions(p[i].title, p[i].text);
+                    createEntryInSuggestions(p[i].title, p[i].text, i);
                 }
 
-                for (let i = 0; i < 8; i++) {
-                    createEntryInSuggestions("StGB $ 223 Körperverletzung", "(1) Wer eine andere Person körperlich mißhandelt oder an der Gesundheit schädigt, wird mit Freiheitsstrafe bis zu fünf Jahren oder mit Geldstrafe bestraft.\n" +
-                        "\n" +
-                        "(2) Der Versuch ist strafbar.");
+                if (p.length === 0) {
+                    for (let i = 0; i < 8; i++) {
+                        createEntryInSuggestions("StGB $ 223 Körperverletzung", "(1) Wer eine andere Person körperlich mißhandelt oder an der Gesundheit schädigt, wird mit Freiheitsstrafe bis zu fünf Jahren oder mit Geldstrafe bestraft.\n" +
+                            "\n" +
+                            "(2) Der Versuch ist strafbar.", i);
+                    }
                 }
+
+                addEventToItems();
             }
         )
     }
@@ -79,10 +98,3 @@ let exampleText = "Um 17:45 Uhr am 17.06.2020 erfolgte die Anweisung durch das F
     "Frau in Empfang genommen. ";
 
 textArea.value = exampleText;
-
-
-const detailText = document.getElementById('detail_text');
-detailText.onclick = function () {
-    console.log('Clicked!');
-    detailText.innerHTML = "";
-};
